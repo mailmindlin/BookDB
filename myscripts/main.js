0e5ebff911f1b8176117ff4f611df9d49762834e
mailmindlin <liamttlyrox@gmail.com>
2014-07-12 14:07:58 -0400

    Moved bookdb

--- myscripts/main.js
+++ myscripts/main.js
@@ -1,5 +1,23 @@
 document.addEventListener('load',function() {
-  var bookDB = (function() {
-  
-  };);
+	window['RemoteGetter'] = function(url, callback) {
+		var self = Object.create(null);
+		
+	};
+
+	
+	function parseDB(file) {
+		var lines = file.split('\n');
+		for(var i=0;i<lines.length;i++) {
+			
+		}
+	}
+	new RemoteGetter("https://rawgit.com/mailmindlin/BookDB/master/files.ref", function(result) {
+		//parse file
+		var urls = result.split('\n');
+		window['bookDB']={};
+		for(var url in urls) {
+			
+		}
+	};);
+	
 },false);
